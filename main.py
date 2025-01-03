c5315dedcac9022567b8620cef8814125a27dcbf
Jnyambok <juliusnyambok14@gmail.com>
2025-01-03 15:59:07 +0300

    Getting there

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import mlflow
 import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from hydra.core.hydra_config import HydraConfig
+
 
 #The @hydra.main() decorator tells Hydra that this is the main function of the script. This decorator is required for Hydra to work.
-@hydra.main(config_path='config')
-def go(config: DictConfig):
+@hydra.main(config_path="conf/config.yaml",version_base=None)
+def go(config: DictConfig)-> None:
 
     #You can get the path at the root of the MLflow project with this:
     root_path = hydra.utils.get_original_cwd()
